7de8102f5a371945927db051983d1b71127cb535
Jan Hrůza <honza.hruza.d5@hotmail.com>
2025-10-24 18:16:26 +0200

    Pridan priklad 10: Lerp

--- priklady/Lerp/main.c
+++ priklady/Lerp/main.c
@@ -17,9 +17,32 @@
 #include <stdio.h>
 
 /// <summary>
+/// Metoda pro vypocet hodnoty linearni interpolace.
+/// </summary>
+/// <param name="v0">Hodnota 1.</param>
+/// <param name="v1">Hodnota 2.</param>
+/// <param name="t">Parametr.</param>
+/// <returns>Hodnotu linearni interpolace podle parametru.</returns>
+double Lerp(double v0,
+            double v1,
+            double t)
+{
+    // vzorec pro vypocet linearni interpolace mezi dvema hodnotami: a + t(b - a)
+    double result = v0 + t*(v1-v0);
+    return result;
+}
+
+/// <summary>
 /// Hlavni program.
 /// </summary>
 int main(void)
 {
+    double v0, v1, t, lerp;
+    v0 = 2;
+    v1 = 6;
+    t = 0.75;
+    lerp = Lerp(v0, v1, t);
+
+    printf("Pro body v0 = %lf, v1 = %lf:\nInterpolovana hodnota v t = %lf je %lf.\n", v0, v1, t, lerp);
     return 0;
 }
